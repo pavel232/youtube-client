1807afcdafabe35adb337ef017fe0735813dafae
Pavel232 <panq.1991@gmail.com>
2020-02-09 14:01:53 +0300

    feat: add application compotents; refactor: create app structure

--- YouTube-Client-App/src/app/app.module.ts
+++ YouTube-Client-App/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { SearchItemComponent } from './components/search-item/search-item.component';
+import { SortingComponent } from './components/header/sorting/sorting.component';
+import { SearchBarComponent } from './components/header/search-bar/search-bar.component';
+import { LoginComponent } from './components/header/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    SortingComponent,
+    SearchBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
